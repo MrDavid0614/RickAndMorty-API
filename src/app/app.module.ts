637f7f1e64924e5361decca6c805baa5a6d80d0f
David <raymondinf23@gmail.com>
2021-04-03 22:46:29 -0400

    Import HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { CharactersComponent } from './components/characters/characters.component';
@@ -18,7 +20,8 @@ import { MenuComponent } from './shared/layout/menu/menu.component';;
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
