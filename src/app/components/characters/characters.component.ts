5d8c907786783f33026ab29438e7c59c1eb23756
David <raymondinf23@gmail.com>
2021-07-23 01:43:17 -0400

    Modularize character components

--- src/app/components/characters/characters.component.ts
+++ src/app/components/characters/characters.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, OnInit, OnDestroy, HostListener, Inject } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Character } from 'src/app/shared/models/character';
 import { CharacterService } from 'src/app/shared/services/character.service';
@@ -11,15 +12,30 @@ import { CharacterService } from 'src/app/shared/services/character.service';
 export class CharactersComponent implements OnInit, OnDestroy {
   querySubscription: Subscription;
   characters: Character[];
+  page: number = 1;
+  showButton: boolean = false;
+  private scrollHeight = 500;
 
-  constructor(private characterService: CharacterService) { }
+  constructor(@Inject(DOCUMENT) private document: Document,
+    private characterService: CharacterService) { }
 
   ngOnInit(): void {
-    this.getCharacters();
+    this.getCharacters(this.page);
   }
 
-  getCharacters(page: number = 1, filter?: any){
+  @HostListener('window:scroll')
+  private onWindowScroll(): void {
+    const yOffset = window.pageYOffset;
+    const scrollTop = this.document.documentElement.scrollTop;
 
+    this.showButton = (yOffset || scrollTop) > this.scrollHeight;
+  }
+
+  onScrollTop(): void {
+    this.document.documentElement.scrollTop = 0;
+  }
+
+  private getCharacters(page: number = 1, filter?: any): void {
     this.querySubscription = this.characterService.getCharacters(page, filter).subscribe((characters: any)=>{
       this.characters = characters;
     })
